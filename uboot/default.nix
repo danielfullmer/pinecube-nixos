c3f86b775fe419ce993d07016201a34ae3fed052
Daniel Fullmer <danielrf12@gmail.com>
2020-10-27 18:52:19 -0700

    uboot: rebase patches on top of Icenowy's

--- uboot/default.nix
+++ uboot/default.nix
@@ -4,6 +4,14 @@ pkgs.buildUBoot {
   patches = [ ./Pine64-PineCube-uboot-support.patch ];
 
   defconfig = "pinecube_defconfig";
+
+  # Putting this here because it's more a design choice and not generic support
+  # for hardware.
+  extraConfig = ''
+    CONFIG_CMD_BOOTMENU=y
+  '';
+
   extraMeta.platforms = ["armv7l-linux"];
   filesToInstall = ["u-boot-sunxi-with-spl.bin"];
+
 }
