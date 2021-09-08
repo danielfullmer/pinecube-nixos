634a9f00788de2270c3a229d711d5e90e3837252
Daniel Fullmer <danielrf12@gmail.com>
2021-09-07 23:28:34 -0700

    WIP

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,11 @@
   ];
 
   boot.kernelModules = [ "spi-nor" ]; # Not sure why this doesn't autoload. Provides SPI NOR at /dev/mtd0
-  boot.extraModulePackages = [ config.boot.kernelPackages.rtl8189es ];
+  #boot.extraModulePackages = [ config.boot.kernelPackages.rtl8189es ];
+
+  #hardware.firmware = with pkgs; [ wireless-regdb rtlwifi_new-firmware ];
+  #hardware.enableRedistributableFirmware = true;
+  #nixpkgs.config.allowUnfree = true;
 
   zramSwap.enable = true; # 128MB is not much to work with
 
@@ -37,7 +41,9 @@
     ffmpeg
     (v4l_utils.override { withGUI = false; })
     usbutils
+    #motion
   ];
+  #++ (with gst_all_1; [ gstreamer gst-plugins-good gst-plugins-bad gst-plugins-ugly ]);
 
   ###
 
@@ -50,8 +56,15 @@
     extraGroups = [ "wheel" "networkmanager" "video" ];
     initialPassword = "nixos";
   };
-  services.mingetty.autologinUser = "nixos";
+  services.getty.autologinUser = "nixos";
 
   networking.wireless.enable = true;
 
+  nixpkgs.overlays = [
+    (self: super: {
+      linuxPackages_5_10 = super.linuxPackages_5_10.extend (_self: _super: {
+        rtl8189es = _super.callPackage ./pkgs/rtl8189es {};
+      });
+    })
+  ];
 }
--- cross-config.nix
+++ cross-config.nix
@@ -11,7 +11,7 @@
   nixpkgs.overlays = [ (self: super: {
     # Dependency minimization for cross-compiling
     cairo = super.cairo.override { glSupport = false; };
-    libass = super.libass.override { encaSupport = false; };
+    #libass = super.libass.override { encaSupport = false; };
     gnutls = super.gnutls.override { guileBindings = false; };
     polkit = super.polkit.override { withIntrospection = false; };
   }) ];
